9b09bf043079e4a30e8267608461ea007cded869
nohkes <41023611+nohkes@users.noreply.github.com>
2018-11-21 21:32:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  // var sums = 0;
+  // for (var i = fromN; i <= toN; i++) {
+  //   sums += i;
+  // }
+  // return sums;
+  if (fromN === toN) {
+    return fromN;
+  }
+  return fromN + sum(fromN + 1, toN);
 }
+// 3,7 = 3 + 4 + 5 + 6 + 7
+// fromN + toN-1
 
 module.exports = sum;
